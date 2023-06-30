8703f8f48b60c9ade0e27fcedec90b55a34a472a
kufita <carrizofacundo644@gmail.com>
2023-06-29 22:34:50 -0300

    complete el trabajo

--- tp4programacion2/ejerciciotp4.ts
+++ tp4programacion2/ejerciciotp4.ts
@@ -33,4 +33,54 @@ class PartidoPolitico {
       this.candidatos.push(candidato);
     }
   }
+  class Votante {
+    nombre: string;
+    edad: number;
+    listaVotada: Lista | null;
   
+    constructor(nombre: string, edad: number) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.listaVotada = null;
+    }
+  
+    emitirVoto(lista: Lista) {
+      this.listaVotada = lista;
+    }
+  }
+  
+  class Voto {
+    votante: Votante;
+    listaVotada: Lista;
+  
+    constructor(votante: Votante, listaVotada: Lista) {
+      this.votante = votante;
+      this.listaVotada = listaVotada;
+    }
+  }
+  
+  // Ejemplo de uso
+  const partido1 = new PartidoPolitico('Partido A');
+  const partido2 = new PartidoPolitico('Partido B');
+  
+  const candidato1 = new Candidato('Candidato 1', partido1);
+  const candidato2 = new Candidato('Candidato 2', partido1);
+  const candidato3 = new Candidato('Candidato 3', partido2);
+  const candidato4 = new Candidato('Candidato 4', partido2);
+  
+  const lista1 = new Lista('Lista 1');
+  lista1.agregarCandidato(candidato1);
+  lista1.agregarCandidato(candidato3);
+  
+  const lista2 = new Lista('Lista 2');
+  lista2.agregarCandidato(candidato2);
+  lista2.agregarCandidato(candidato4);
+  
+  const votante1 = new Votante('Votante 1', 30);
+  const votante2 = new Votante('Votante 2', 25);
+  
+  votante1.emitirVoto(lista1);
+  votante2.emitirVoto(lista2);
+  
+  const voto1 = new Voto(votante1, lista1);
+  const voto2 = new Voto(votante2, lista2);
